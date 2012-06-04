97581ce7177fc1f8831d75f639d8b08206fca9db
Craig M. Stimmel <cstimmel@uw.edu>
2012-06-04 16:52:44 -0700

    Adding a python script so I can do some pre-commit hook testing.

---
+++ lala.py
@@ -0,0 +1,11 @@
+""" A badly formatted python script for testing a pre-commit hook. """
+
+# This is a really long comment so that PEP8 gets upset about it. It thinks lines should only be 80 chars or less.
+
+def main():
+    name = "Craig"
+    string="Hi %s" % name
+    return string
+
+if __name__=="__main__":
+    print main()
