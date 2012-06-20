0b7b2c8795c04cc6fa7a498be3dbf309c99f367b
Craig M. Stimmel <cstimmel@uw.edu>
2012-06-20 11:00:20 -0700

    Commit for testing gitorious.

--- lala.py
+++ lala.py
@@ -2,9 +2,12 @@
 
 # This is a really long comment so that PEP8 gets upset about it. It thinks lines should only be 80 chars or less.
 
+
 def main():
     name = "Craig"
     string="Hi %s" % name
+    import random
+    randnum = random.randrange(1, 99)
     return string
 
 if __name__=="__main__":
